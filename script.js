fabe875343709a6506976747c79213704685ab97
Joseph Coulter <joeycoulter@gmail.com>
2021-10-10 21:43:15 -0700

    Beginning computer opponenet logic

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 /* 
                 To-Do
 
-    [ ] Try switching row/column/diagonal functions into game module.
-    [ ] Figure out object of row/column/diagonal functions, loop through for results check.
+    [x] Try switching row/column/diagonal functions into game module.
+    [x] Figure out object of row/column/diagonal functions, loop through for results check.
+    [ ] Make game.checkResults() return finalResult, have displayController check when this value
+        equals something other than undefined then display that result via a div toggle with
+        win message in body.
+        --- returning finalResult from game module gets a referenceError: finalResult is not defined..
+            figure this out!!!
 
 
 */
@@ -10,9 +15,9 @@
 
 
 const gameboard = (() => {
-    let board = ['', '', '',
-                 '', '', '',
-                 '', '', ''];
+    let board = [0, 1, 2,
+                 3, 4, 5,
+                 6, 7, 8];
     
     const resultCheckSectors = {
 
@@ -26,21 +31,26 @@ const gameboard = (() => {
         diagonal2: () => [2,4,6].map(x => board[x]),
     }
     
+    function getRemainingSectors() {
+        return board.filter(i => i != `o`&& i != `x`);
+    }
 
     const getBoard = () => board;
     function setBoard(index, marker) {
         board.splice(index, 1, marker);
-    }
+    }                   
+
 
     return { 
-        // row1, row2, row3, column1, column2,column3, diagonal1, diagonal2,
         resultCheckSectors,
+        getRemainingSectors,
         getBoard: getBoard,
         setBoard: setBoard
     }
 })();
 
 const game = (() => {
+    
     let whoseTurn = 0;
     const getTurn = () => whoseTurn;
     const setTurn = () => whoseTurn++;
@@ -48,46 +58,112 @@ const game = (() => {
         
     }
     
-    
+    const  computerOpponent = () => {
+            randomSectorSelect = gameboard.getRemainingSectors()[Math.floor(Math.random() * gameboard.getRemainingSectors().length)]
+            console.log(randomSectorSelect);
+            randomEmptyIndex = gameboard.getBoard()[randomSectorSelect];
+            console.log(randomEmptyIndex)
+            let computerChoice = document.querySelector(`#sector-${randomEmptyIndex}`);
+            console.log(computerChoice)
+            computerChoice.textContent = playerTwo.marker;
+            gameboard.setBoard(randomEmptyIndex, playerTwo.marker);
+            game.checkResults();
+            game.setTurn();
+            console.log(`work`);
+    }
 
     const checkResults = () => {
-        // Object.keys(gameboard.resultCheckSectors).forEach(key => {
-            
-        // });
-        
+
         for (let key in gameboard.resultCheckSectors) {
             
-            // if ( gameboard.resultCheckSectors[`${key}`]() == [`x`, 'x', 'x'] || [`o`, 'o', 'o']) {
-            //     let finalResult = gameboard.resultCheckSectors[`${key}`];
-            //     return finalResult
-            // }
-            // return finalResult'
+            let finalResult;
             if (gameboard.resultCheckSectors[`${key}`]().join(``) === `xxx` ||
                 gameboard.resultCheckSectors[`${key}`]().join(``) === `ooo`) {
                 if (whoseTurn % 2 === 0) {
                     console.log(`playerOne wins!`)
+                    return finalResult = `playerOne wins!`
                 } else if (whoseTurn % 2 !== 0) {
                     console.log(`playerTwo wins!`)
+                    return finalResult = `playerTwo wins!`
                 }
                 
             // improve this logic, it isn't catching the case of a win in the last turn
-            } else if (whoseTurn === 8 && (
-                gameboard.resultCheckSectors[`${key}`]().join(``) !== `xxx` ||
-                gameboard.resultCheckSectors[`${key}`]().join(``) !== `ooo`))
-            {
+            } else if ((whoseTurn === 8) &&
+                (gameboard.resultCheckSectors[`${key}`]().join(``) != `xxx`) &&
+                (gameboard.resultCheckSectors[`${key}`]().join(``) != `ooo`))
+                {
                 console.log(`it's a tie!`);
-                break;
-                
+                return finalResult = `it's a tie!`
             } else {
                 console.log(`no matches`);
             }
         }
-        // console.log(finalResult);
-
     }
+
+    function minimax() {
+        const emptyCellsStore = getRemainingSectors(board);
+        
+        if (finalResult == `playerOne wins!`) {
+            return {score: -1};
+        } else if (finalResult == `playerTwo wins!`) {
+            return {score: 1};
+        } else if (emptyCellsStore.length === 0) {
+            return {score: 0};
+        }
+
+        const testHistory = [];
+
+        for (let i = 0; i < emptyCellsStore.length; i++) {
+            
+            const currentTest = [];
+
+            currentTest.index = board[emptyCellsStore[i]];
+
+            board[emptyCellsStore[i]] = playerTwo.marker;
+
+            if (whoseTurn % 2 !== 0) {
+                const result = minimax(board);
+
+                currentTest.score = result.score;
+            } else {
+                const result = minimax(board);
+                currentTest.score = result.score;
+            }
+
+            board[emptyCellsStore[i]] = currentTest.index;
+
+            testHistory.push(currentTest);
+        }
+
+        let bestNextMove;
+
+        if (whoseTurn % 2 !== 0) {
+            let bestScore = -Infinity;
+            for (let i = 0; i < testHistory.length; i++) {
+                if (testHistory[i].score > bestScore) {
+                    bestScore = testHistory[i].score;
+                    bestNextMove = i;
+                }
+            }
+        } else {
+            let bestScore = Infinity;
+            for (let i = 0; i < testHistory.length; i++) {
+                if (testHistory[i].score < bestScore) {
+                    bestScore = testHistory[i].score;
+                    bestNextMove = i;
+                }
+            }
+        }
+
+        return testHistory[bestNextMove];
+    }
+
+
 
     return { 
         checkResults,
+        computerOpponent,
+        // finalResult,
         getTurn,
         setTurn 
     }
@@ -102,23 +178,37 @@ const displayController = (() => {
         // const boardSpace = document.getElementById(`${i}`);
         // 
 
-        for (let i = 0; i<= 8; i++) {
+        for (let i = 0; i <= 8; i++) {
             const boardSpace = document.createElement(`div`);
             boardSpace.classList.add(`board-space`);
-            boardSpace.setAttribute(`id`, `space-${i}`);
+            boardSpace.setAttribute(`id`, `sector-${i}`);
             gameboardContainer.appendChild(boardSpace);
 
             boardSpace.addEventListener(`click`, () => {
                 
-                if (game.getTurn() % 2 == 0 && boardSpace.textContent == ``) {
-                    boardSpace.textContent = playerOne.marker;
-                } else if (boardSpace.textContent == ``) {
+                if (game.getTurn() % 2 == 0 && boardSpace.textContent !== `x` && boardSpace.textContent !== `o` && playerTwo.name == `computer`) {
+                    boardSpace.textContent = playerOne.marker;       
+                    gameboard.setBoard(i, boardSpace.textContent);
+                    game.checkResults();
+                    game.setTurn();
+                    game.computerOpponent();
+
+                } else if (game.getTurn() % 2 == 0 && boardSpace.textContent !== `x` && boardSpace.textContent !== `o`) {
+                    boardSpace.textContent = playerOne.marker;           
+                    gameboard.setBoard(i, boardSpace.textContent);
+                    game.checkResults();
+                    game.setTurn(); 
+
+                
+                } else if (boardSpace.textContent !== `x` && boardSpace.textContent !== `o`) {
                     boardSpace.textContent = playerTwo.marker;
+                    gameboard.setBoard(i, boardSpace.textContent);
+                    game.checkResults();
+                    game.setTurn();
+
                 }
                 
-                gameboard.setBoard(i, boardSpace.textContent);
-                game.checkResults();
-                game.setTurn();
+                
             })
         } 
     };
@@ -129,7 +219,8 @@ const displayController = (() => {
     }
 })();
 
-const Player = (player1Or2, name, marker) => {
+const Player = (player1Or2, name, marker) => 
+{
     return {
         player1Or2,
         name, 
@@ -144,8 +235,3 @@ let playerTwo = Player(2, `computer`, `o`)
 
 displayController.displayBoard();
 
-// I need to split my games functionality between gameboard, game, and player objects.
-// I must create a function that allows players to add marks to a specific spot while
-// keeping them from marking over already-marked-spots. I also need to create the game logic
-// which will track which player is making a move, names of players, and their designated `x` or
-// `o` marks. 
