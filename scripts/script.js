462d3eb90ba44e9d18a806776c9939f1902c6dac
Joseph Coulter <joeycoulter@gmail.com>
2021-11-01 16:54:26 -0700

    Added: Difficulty selection UI, gameboard animations

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,7 @@
     [ ] form validation + lock other radio buttons when choice is made.
     [ ] game difficulty selector  
     [ ] Purely aesthetic UI completion (backgrounds, animations, music, sound effects)
-        [ ] animate background of gameboard to cycle through each color of my Hyperdrive
+        [x] animate background of gameboard to cycle through each color of my Hyperdrive
             pallete on coolors.co (fast loop), adjust opacity!
     [x] Put get player info functions in Player factory possibly?
  
@@ -29,8 +29,8 @@ const Player = (number, name, marker) => {
     const getNumber = () => number;
     const getName = () => name;
     const getMarker = () => marker;
-    
-    
+
+
     return {
         getNumber,
         getName,
@@ -42,12 +42,12 @@ const Player = (number, name, marker) => {
 
 
 const gameboard = (() => {
-    
-    let _board = [0, 1, 2,
-                  3, 4, 5,
-                  6, 7, 8];
 
-    
+    let _board = [0, 1, 2,
+        3, 4, 5,
+        6, 7, 8];
+
+
     const _resultCheckSectors = {
 
         row1: () => [0, 1, 2].map(x => _board[x]),
@@ -74,12 +74,12 @@ const gameboard = (() => {
 
 
     return {
-        
+
         getResultCheckSectors,
         getRemainingSectors,
         getBoard,
         setBoard,
-    
+
     }
 })();
 
@@ -91,11 +91,17 @@ const game = (() => {
     let playerOne;
     let playerTwo;
     let result = null;
+    let compDifficulty = `new on the job`;
+
+    const setCompDifficulty = (value) => {
+        compDifficulty = value;
+        console.log(compDifficulty);
+    }
 
     function setPlayer(player) {
         if (player.getNumber() === 1) {
             playerOne = player
-            return playerOne; 
+            return playerOne;
         } else if (player.getNumber() === 2) {
             playerTwo = player;
             return playerTwo;
@@ -120,7 +126,7 @@ const game = (() => {
 
     const getTurn = () => _whoseTurn;
     const setTurn = () => _whoseTurn++;
-    
+
 
 
     const _compMM = () => {
@@ -144,12 +150,12 @@ const game = (() => {
 
     const _compChoiceLogic = () => {
 
-        let compDifficulty = `in training`;
+        console.log(compDifficulty)
 
-        if (compDifficulty === `unbeatable`) {
+        if (compDifficulty === `unstoppable`) {
 
             return _compMM();
-        } else if (compDifficulty !== `unbeatable`) {
+        } else if (compDifficulty !== `unstoppable`) {
             let rNum = Math.random() * 100;
 
             if (compDifficulty === `highly skilled`) {
@@ -163,7 +169,7 @@ const game = (() => {
             } else if (compDifficulty === `in training`) {
 
                 if (rNum < 50) {
-                    
+
                     return _compMM();
                 } else {
 
@@ -183,15 +189,14 @@ const game = (() => {
 
 
 
-
     function checkResults(playerMarker) {
         for (let key in gameboard.getResultCheckSectors()) {
 
             if (gameboard.getResultCheckSectors()[`${key}`]().join(`,`) === `${playerMarker},${playerMarker},${playerMarker}`) {
-                
+
                 return true
 
-            } 
+            }
         }
 
     }
@@ -199,8 +204,8 @@ const game = (() => {
     const finalWinCheck = () => {
         if (checkResults(playerOne.getMarker())) {
             alert(`player 1 win`);
-            
-            
+
+
         } else if (checkResults(playerTwo.getMarker())) {
             if (playerTwo.getName() === `computer`) {
                 alert(`computer win`)
@@ -211,7 +216,7 @@ const game = (() => {
     }
 
     function minimax(boardState, playerMarker) {
-        
+
         const getEmptySectors = gameboard.getRemainingSectors(boardState);
         let score;
 
@@ -268,6 +273,7 @@ const game = (() => {
 
     return {
         checkResults,
+        setCompDifficulty,
         computerOpponent,
         getTurn,
         setTurn,
@@ -283,6 +289,7 @@ const game = (() => {
 
 
 const displayController = (() => {
+    
     const gameboardContainer = document.querySelector(`#gameboard-container`);
     const enterBtn = document.querySelector(`#enter-btn`);
     const startGameBtn = document.querySelector(`#start-btn`);
@@ -291,7 +298,11 @@ const displayController = (() => {
     const playerTwoIcon = document.querySelector(`#player-two-icon`);
     const playerOneName = document.querySelector(`#player-one-name`);
     const playerTwoName = document.querySelector(`#player-two-name`);
-    
+    const startGameContainer = document.querySelector(`#start-game-container`)
+    let difficulty;
+
+    const getDifficulty = () => difficulty;
+
     function displayBoard() {
 
         function playTurn(playerNumber, turnSpace, index) {
@@ -333,13 +344,13 @@ const displayController = (() => {
 
     function setPlayerMarker(radioGroup) {
         let element = document.getElementsByClassName(radioGroup);
-        
+
         for (const elements of element) {
             if (elements.checked) {
                 console.log(elements.checked)
                 return elements.value;
             } else {
-            continue
+                continue
             }
         }
     };
@@ -356,15 +367,16 @@ const displayController = (() => {
             introToPlayerSelect
                 .to("#enter-btn", { duration: 1, xPercent: -150 }, 0)
                 .to("#intro-screen", { duration: .8, xPercent: -150 }, .3)
-                .to("#player-select-form", { duration: 1, xPercent: -150.2, yPercent: 0 }, 1);
+                .to("#player-select-form", { duration: 1, xPercent: -150.2, yPercent: 0 }, 1)
+                .to("#intro-container", { autoAlpha: 0, delay: 3 });
 
         });
 
         // form control
 
-        
+
         const introAnimation = gsap.timeline();
-            introAnimation
+        introAnimation
             .to("#intro-title", { duration: 1.5, rotate: 720, perspective: 500, scale: 50, ease: "back.out" })
             .from("#enter-btn", { duration: 1, x: 1000 });
 
@@ -375,50 +387,80 @@ const displayController = (() => {
                 if (elements.checked) {
                     console.log(elements.checked)
                     value = elements.value;
-
                     if (value === `player`) {
+                        if (startGameContainer.childNodes.length > 1) {
+                            startGameContainer.removeChild(difficultySelector);
+                            startGameContainer.removeChild(difficultySelectorLabel)
+                        }
                         playerTwoIcon.src = "/images/player-two-icon.png";
                         playerTwoName.value = ``;
                         playerTwoName.disabled = false;
                     } else if (value === `computer`) {
-                        playerTwoIcon.src ="/images/ai-icon.png"
+                        playerTwoIcon.src = "/images/ai-icon.png"
                         playerTwoName.value = `computer`;
                         playerTwoName.disabled = true;
+                        difficultySelectorLabel = document.createElement(`p`);
+                        difficultySelectorLabel.textContent = `Computer Difficulty`;
+                        difficultySelectorLabel.classList.add(`selector-label`)
+                        difficultySelector = document.createElement(`button`);
+                        difficultySelector.setAttribute(`id`, `difficulty-selector`);
+                        difficultySelector.textContent = `new on the job`
+                        startGameContainer.insertBefore(difficultySelector, startGameBtn);
+                        startGameContainer.insertBefore(difficultySelectorLabel, difficultySelector)
+                        let count = 0;
+                        
+                        difficultySelector.addEventListener(`click`, () => {
+                            let difficulties = [`new on the job`, `in training`, `highly skilled`, `unstoppable`]
+                            console.log(count)
+                            if (count < 3) {
+                                count += 1;
+                                difficulty = difficulties[count];
+                                difficultySelector.textContent = difficulty;
+                                console.log(count);
+                            } else {
+                                count = 0;
+                                difficulty = difficulties[count];
+                                difficultySelector.textContent = difficulty;
+                                console.log(count)
+                            }
+                            game.setCompDifficulty(difficulty);
+                        })
                     }
                 } else {
-                continue
+                    continue
                 }
             }
         }
 
 
-       computerSwitch.forEach(function(element) {
-           element.addEventListener(`change`, () => playerToggle())
-           console.log(element)
+        computerSwitch.forEach(function (element) {
+            element.addEventListener(`change`, () => playerToggle())
+            console.log(element)
 
-       })
-           
-            
-            
+        })
 
-       
         
-        playerSelectForm.addEventListener(`submit`, function (event) { 
-            event.preventDefault(); 
+
+
+
+
+
+        playerSelectForm.addEventListener(`submit`, function (event) {
+            event.preventDefault();
             playerOneMarker = setPlayerMarker(`player-one-radio`);
             playerTwoMarker = setPlayerMarker(`player-two-radio`);
             p1Name = playerOneName.value;
             p2Name = playerTwoName.value
 
             playerOne = Player(1, p1Name, playerOneMarker);
-            
+
             playerTwo = Player(2, p2Name, playerTwoMarker);
 
             game.setPlayer(playerOne);
-            
+
             game.setPlayer(playerTwo);
 
-            
+
 
         });
 
@@ -427,23 +469,62 @@ const displayController = (() => {
 
         startGameBtn.addEventListener(`click`, () => {
             const startGameAnimation = gsap.timeline();
-            const boardAnimation = gsap.timeline({
+            const boardSpaceAnimation = gsap.timeline({
                 repeat: -1,
+                repeatDelay: 0,
+                defaults: {
+                    ease: "power4",
+                    
+                }
+            })
+            const boardScaleAnimation = gsap.timeline({
+                repeat: -1,
+                repeatDelay: 0,
+                defaults: {
+                    ease: "power1.out"
+                }
             });
 
-            boardAnimation.timeScale(20);
+            const boardSpinAnimation = gsap.timeline({
+                repeat: -1,
+                repeatDelay: 0,
+                defaults: {
+                    ease: "power1.out"
+                }
+            })
+
+            const spaceDuration = 60 / 130;
+
+            
+
 
             startGameAnimation
                 .to("#player-select-form", { duration: 1, xPercent: -300 }, 0)
+                .to("#player-select-form", { autoAlpha: 0 }, 3)
+
                 .to("#game-container", { duration: 1, xPercent: -300, yPercent: -35, ease: "bounce" }, 1);
-            
-            boardAnimation
-                .to(".board-space", {duration: 1, background: "hsl(84, 58, 71)"})
-                .to(".board-space", {duration: 1, background: "hsl(65, 44, 67)"})
-                .to(".board-space", {duration: 1, background: "hsl(45, 51, 67)"})
-                .to(".board-space", {duration: 1, background: "hsl(32, 64, 69)"})
-                .to(".board-space", {duration: 1, background: "hsl(22, 78, 70)"})
-                .to(".board-space", {duration: 1, background: "hsl(15, 93, 71)"});
+
+            boardSpaceAnimation
+                .to(".board-space", { background: "hsla(82, 100%, 55%, .85)" }, 0.61538)
+                .to(".board-space", { background: "hsla(43, 100%, 53%, .85)" })
+                .to(".board-space", { background: "hsla(30, 100%, 54%, .85)" })
+                .to(".board-space", { background: "hsla(20, 100%, 55%, .85)" })
+                .to(".board-space", { background: "hsla(13, 100%, 55%, .85)" })
+                .totalDuration(spaceDuration * 8);
+
+            boardScaleAnimation
+                .to("#gameboard-container", { scale: 1.1 })
+                .totalDuration(spaceDuration);
+
+
+            boardSpinAnimation
+
+                .to("#gameboard-container", { rotation: 90 }, 2)
+                .to("#gameboard-container", { rotation: 180 }, 4)
+                .to("#gameboard-container", { rotation: 270 }, 6)
+                .to("#gameboard-container", { rotation: 360 }, 8)
+                .totalDuration(spaceDuration * 16);
+
 
         })
     }
@@ -454,11 +535,11 @@ const displayController = (() => {
 
 
     return {
-        
+        getDifficulty,
         displayBoard,
         menuController,
         setPlayerMarker,
-        }
+    }
 })();
 
 
