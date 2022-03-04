213251201c56c66bd541ef5fa8d5ba7d5531c0e8
Joseph Coulter <joeycoulter@gmail.com>
2022-03-03 20:22:12 -0800

    Production mode webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  mode: "development",
+  mode: "production",
   plugins: [
     new HtmlWebpackPlugin({
       inject: false,
